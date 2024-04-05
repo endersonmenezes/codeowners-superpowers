29bb8aacac83cd1031689b247c8263fdc13944b8
Enderson Menezes <endersonster@gmail.com>
2024-04-05 18:13:50 -0300

    Update PR_URL variable to use $PR_NUMBER instead of $PR_URL

--- main.sh
+++ main.sh
@@ -15,7 +15,8 @@ PR_URL="https://github.com/$OWNER/$REPOSITORY/pull/$PR_NUMBER"
 echo "Analyzing PR: $PR_URL"
 
 ## Get Changed Files
-gh pr diff --name-only $PR_URL > changed_files.txt
+gh pr checkout $PR_URL
+gh pr diff --name-only $NUMBER > changed_files.txt
 echo 
 echo "Changed Files:"
 cat changed_files.txt
@@ -80,7 +81,7 @@ for OWNER in "${NECESSARY_APPROVALS[@]}"; do
     echo $OWNER
 done
 
-PR_APPROVED=$(gh pr view $PR_URL --json reviews | jq '.reviews[] | select(.state == "APPROVED") | .author.login')
+PR_APPROVED=$(gh pr view $PR_NUMBER --json reviews | jq '.reviews[] | select(.state == "APPROVED") | .author.login')
 PR_APPROVED=$(echo $PR_APPROVED | tr -d '"')
 
 echo 
